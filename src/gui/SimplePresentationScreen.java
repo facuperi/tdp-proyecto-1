ef1d94644d62dc2f0f6321e194a1f7220f8f4362
facuperi <facuelperi@gmail.com>
2022-09-01 21:41:24 -0300

    fourth commit gui corregida

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -1,23 +1,23 @@
 package gui;
-import javax.swing.JFrame;
 
-
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.time.LocalDateTime;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import entities.Student;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import java.awt.Toolkit;
-import javax.swing.SwingConstants;
 
-@SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
 
 	private JPanel contentPane;
@@ -26,16 +26,16 @@ public class SimplePresentationScreen extends JFrame {
 	private Student studentData;
 	private JTextField tfLU,tfApellido,tfNombre,tfMail,tfURL;
 	private JLabel lblLU,lblApellido,lblNombre,lblMail,lblURL,lblFecha,lblImagen;
-	
-	public SimplePresentationScreen(Student studentData) {
-		this.studentData = studentData;
-		
+
+	/**
+	 * Create the frame.
+	 */
+	public SimplePresentationScreen(Student studData) {
+		studentData = studData;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 622, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentación");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(new Dimension(615, 280));
-		setResizable(false);
 		setContentPane(contentPane);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png")));
 		
@@ -43,6 +43,7 @@ public class SimplePresentationScreen extends JFrame {
 	}
 	
 	private void init() {
+		contentPane.setLayout(null);
 		contentPane.setLayout(null);
 		
 		// Tabbed Pane to student personal data
@@ -55,7 +56,7 @@ public class SimplePresentationScreen extends JFrame {
 		
 		
 		tfLU = new JTextField();
-		tfLU.setBounds(80, 10, 332, 19);
+		tfLU.setBounds(80, 10, 284, 19);
 		tabInformation.add(tfLU);
 		tfLU.setColumns(10);
 		tfLU.setText(Integer.toString(studentData.getId()));
@@ -65,25 +66,25 @@ public class SimplePresentationScreen extends JFrame {
 		tabInformation.add(lblLU);
 		
 		tfApellido = new JTextField();
-		tfApellido.setBounds(80, 39, 315, 19);
+		tfApellido.setBounds(80, 39, 284, 19);
 		tfApellido.setColumns(10);
 		tabInformation.add(tfApellido);
 		tfApellido.setText(studentData.getLastName());
 		
 		tfNombre = new JTextField();
-		tfNombre.setBounds(80, 68, 315, 19);
+		tfNombre.setBounds(80, 68, 284, 19);
 		tfNombre.setColumns(10);
 		tabInformation.add(tfNombre);
 		tfNombre.setText(studentData.getFirstName());
 		
 		tfMail = new JTextField();
-		tfMail.setBounds(80, 97, 315, 19);
+		tfMail.setBounds(80, 97, 284, 19);
 		tfMail.setColumns(10);
 		tabInformation.add(tfMail);
 		tfMail.setText(studentData.getMail());
 		
 		tfURL = new JTextField();
-		tfURL.setBounds(80, 126, 315, 19);
+		tfURL.setBounds(80, 126, 284, 19);
 		tfURL.setColumns(10);
 		tabInformation.add(tfURL);
 		tfURL.setText(studentData.getGithubURL());
@@ -106,23 +107,20 @@ public class SimplePresentationScreen extends JFrame {
 		contentPane.add(tabbedPane);
 		
 		lblFecha = new JLabel("");
-		lblFecha.setBounds(20, 220, 430, 25);
+		lblFecha.setBounds(20, 228, 430, 25);
 		contentPane.add(lblFecha);
 	
 		LocalDateTime tiempo = LocalDateTime.now();
-		int horas  = tiempo.getHour();
-		int minutos = tiempo.getMinute();
-		int segundos = tiempo.getSecond();
-		int dia = tiempo.getDayOfMonth();
-		int mes = tiempo.getMonthValue();
-		int año = tiempo.getYear();
-		lblFecha.setText("Esta ventana fue generada el " + dia + "/" + mes + "/" + año + " a las: " +  horas  + ":"+ minutos +":"+segundos );
+		int horas,minutos,segundos,dia,mes,anio;
+		horas  = tiempo.getHour(); minutos = tiempo.getMinute(); segundos = tiempo.getSecond();
+		dia = tiempo.getDayOfMonth(); mes = tiempo.getMonthValue(); anio = tiempo.getYear();
+		lblFecha.setText("Esta ventana fue generada el " + dia + "/" + mes + "/" + anio + " a las: " +  horas  + ":"+ minutos +":"+segundos );
 		
 		lblImagen = new JLabel("");
 		lblImagen.setHorizontalAlignment(SwingConstants.CENTER);
 		Image img = new ImageIcon(this.getClass().getResource(studentData.getPathPhoto())).getImage();
 		lblImagen.setIcon(new ImageIcon(img.getScaledInstance(275, 155,  java.awt.Image.SCALE_SMOOTH)));
-		lblImagen.setBounds(420, 26, 130, 140);
+		lblImagen.setBounds(445, 27, 130, 181);
 		contentPane.add(lblImagen);
 	
 	}
